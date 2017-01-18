b4fbb2286a2e4614312190daf999d2997f72e435
Kyle Thomson <idynkydnk@gmail.com>
2017-01-17 22:35:24 -0500

    progrresss

--- game_board.rb
+++ game_board.rb
@@ -1,3 +1,4 @@
+require_relative 'graph.rb'
 class Cell
   attr_accessor :value
 
@@ -38,13 +39,30 @@ class GameBoard
     if !location_valid?(start_loc) || !location_valid?(end_loc)
       puts "locations not valid"
     else
+      moves = Graph.new
+      moves.add_node(start_loc, nil)
+      next_move = Node.new([2,3])
+      moves.add_node(next_move, moves.root)
+      moves.add_node(next_move, moves.root)
 
+      puts moves.root.children
     end
   end
 
+  def add_possible_moves(start_loc, end_loc, moves)
+    if start_loc == end_loc
+      return moves
+    end
+    possible_knights_moves = [[1,2], [1,-2], [-1,2], [-1,-2], [2,1], [2,-1], [-2,1], [-2,-1]] 
+    possible_knights_moves.each do |possible_move|
+      next_move = [start_loc[0] + possible_move[0],start_loc[1] + possible_move[1]]
+      if location_valid?(next_move) && !moves.include?(next_move)
+        moves << next_move
+        add_possible_moves(moves.last, end_loc, moves)
+      end
+    end
+    return moves
+  end
 end
-
-
 x = GameBoard.new
-x.knight_moves([2,4], [1,8])
-x.knight_moves([1,1], [0,4])
+x.knight_moves([1,1],[2,3])
